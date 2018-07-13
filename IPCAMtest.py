08ed105c8d82216cbd053d4bcce0ce9b84280b6b
DSUTensor <nndk0043@yahoo.co.kr>
2018-07-14 08:21:48 +0900

    Update IPCAMtest.py

--- IPCAMtest.py
+++ IPCAMtest.py
@@ -1,11 +1,11 @@
-'''import sys
+import sys
 if sys.version_info[0] == 3:
     from urllib.request import urlopen
 else:
     from urllib import urlopen
 import cv2
 import numpy as np
-url = 'http://192.168.0.5:8080/shot.jpg’
+url = 'http://192.168.0.5:8080/shot.jpg'
 while(True):
     imgResp =urlib.urlopen(url)
     imgNp1=np.array(bytearray(imgResp.read()),dtype=np.uint8)
@@ -13,10 +13,10 @@ while(True):
     cv2.imshow('img', img1)    
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break        
-cv2.imwrite(img1,'img.jpg')
-cv2.destroyAllWindows()'''
+cv2.imwrite('img.jpg',img1)
+cv2.destroyAllWindows()
 
-import cv2
+'''import cv2
 import numpy as np
 
 cam = cv2.VideoCapture()
@@ -26,6 +26,6 @@ cv2.imshow('img',img)
     
 if cv2.waitKey(1
 
-cv2.imwrite(img,'img.jpg')
+cv2.imwrite('img.jpg',img)
 cam.release()
-cv2.destroyAllWindows()
+cv2.destroyAllWindows()'''
